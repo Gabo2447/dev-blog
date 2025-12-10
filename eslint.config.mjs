b82472918b8e09b02aeb9e8c6c6acdd7587e84db
Gabo2447 <gabrieldeolival2@gmail.com>
2025-12-09 22:38:49 -0400

    linter: Adding the linter for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
+// eslint.config.js
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+// IMPORTAR PLUGINS Y REGLAS (Paso clave para la nueva API)
+import testingLibrary from "eslint-plugin-testing-library";
+import jestDom from "eslint-plugin-jest-dom";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    files: ["**/*.test.{js,jsx,ts,tsx}"],
+
+    plugins: {
+      "testing-library": testingLibrary,
+      "jest-dom": jestDom,
+    },
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
