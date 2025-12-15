59b706b0b73ec87de2a0a05ec81cbb2062bbecd1
Gabo2447 <gabrieldeolival2@gmail.com>
2025-12-14 23:00:13 -0400

    feat: :tada: Project started with NextJS, Shadcn & cacheComponents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Onest } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistOnest = Onest({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -23,12 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="dark">
+      <body className={`${geistOnest.variable} antialiased`}>{children}</body>
     </html>
   );
 }
