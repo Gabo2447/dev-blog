0f62f1fca5e300f9e863c5ec5d5efb892d9947ce
Gabo2447 <gabrieldeolival2@gmail.com>
2025-12-16 03:12:28 -0400

    refactor: :recycle: Simplify layout and page components by extracting styles into constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+// import { Suspense } from "react"; // Por ahora no se va a usar...
 import dynamic from "next/dynamic";
 const Hero = dynamic(() => import("@/components/home/hero"));
+const Article = dynamic(() => import("@/components/home/article"));
+
+export const metadata = {
+  title: `GL | Dev Blog`,
+  description: "Blog personal de desarrollo web y tecnología.",
+};
+
+const styles = {
+  container: "container mx-auto px-4 sm:px-16",
+};
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 sm:px-16">
+    <div className={styles.container}>
       <Hero />
+      <Article />
     </div>
   );
 }
